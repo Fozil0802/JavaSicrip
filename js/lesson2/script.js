2ae51d9abae33ca15649410cecf7f950ac60d5fb
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-11-08 03:31:50 +0900

    lesson2.dars

---
+++ js/lesson2/script.js
@@ -0,0 +1,137 @@
+// implict automatic
+
+// let result = '5' + 10;
+// console.log(typeof result);
+
+// let result = '5' + true;
+// console.log(typeof result);
+
+// let result = '5' + undefined;
+// console.log(typeof result);
+
+// let result = '4' - '3'
+// console.log(typeof result);
+
+// let  result = 5 -'2';
+// console.log(typeof result);
+
+// let result = 4 * '3';
+// console.log(typeof result);
+
+// let result = 4 / '2';
+// console.log(typeof result);
+
+// let result = 4 - true;
+// console.log(typeof result);let 
+
+// result = 4 + true;
+// console.log(result);
+
+// true=1
+// false=0, null, undefined, ...
+
+// let result = 4 + undefined;
+// console.log(result);
+
+// let result = true + undefined;
+// let result = null + undefined;
+
+// console.log(result);
+
+// Manual
+
+// let result = Number('1995');
+// let result = Number(true)
+// let result = Number(false)
+// let result = Number(null)
+// let result = Number()
+// let result = Number('hello');
+
+// let result = parseInt('95.05')
+// let result = + '95.05'
+
+// let result = String(201)
+// console.log(101);
+// let result = String(3 + 4 + 5)
+// let result = String(true)
+
+// to boolean
+
+// false
+// let result = Boolean(' ')
+// let result = Boolean(1)
+// let result = Boolean(0)
+// let result = Boolean(null)
+// let result = Boolean(undefined)
+// let result = Boolean(NaN)
+
+// let result = Boolean(1)
+// let result = Boolean(200)
+// let result = Boolean('salom')
+// let result = Boolean(' ')
+
+// console.log(result);
+
+// let a = 20;
+// // a = a + 15;
+// // a +=15;
+// // natija birxil
+
+// console.log(a);
+
+// comparison and logical operators
+// comparison => ==, ===, <, >, <=, >=
+// console.log(4 == 4);
+// console.log(4 == 5);
+
+// console.log(4 == '4');true
+// console.log(4 === '4');false
+// console.log(5 >= 5);true
+// console.log(5<3);false
+
+// logical
+
+// let car = 'jentra'
+// let car1 = 'bmw'
+// car === 'jentra' && car1 === 'bmw' && console.log('1995$');
+
+// let login = 'java';
+// let parol = 'abc';
+// if(login === 'java' && parol==='abc'){
+//     console.log('sahifangizga hush kelibsiz');
+// }
+// else{
+//     console.log('parol yoki login xato');
+// }
+
+// let login = 'java';
+// let parol = 'abc';
+// if(login === 'java' ||  parol==='abc'){
+//     console.log('sahifangizga hush kelibsiz');
+// }
+// else{
+//     console.log('parol yoki login xato');
+// }
+
+// console.log(true == true);
+// console.log(4 == 5 || 5 == 5 || 5 == 6);
+
+// console.log(1 && true && false); false
+// console.log(0 && true && false); 0
+// console.log(1 && false && 0); false
+// console.log(1 && true && 0); 0
+
+// console.log(1 || true || false); 1
+// console.log(false || true || 0); true
+// console.log(false || 0 || true); true
+
+// logical not
+// let car = 'nexia';
+// car = 'nexia' && console.log('10000$');
+// car != 'nexia' && console.log('bundan moshina topilmadi');
+
+// console.log(!false);true
+// console.log(!true);false
+
+// console.log(!0);true
+// console.log(!1);false
