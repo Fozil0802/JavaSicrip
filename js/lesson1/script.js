d51b08478f4a006abc280fdd4fa2cf6c9adee78f
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-11-08 01:04:56 +0900

    lesson1

--- js/lesson1/script.js
+++ js/lesson1/script.js
@@ -1,5 +1,5 @@
-// var hello = "salom, dunyolar";
-// console.log(hello);
+var hello = "salom, dunyolar";
+console.log(hello);
 
 console.log(19950802);
 console.log('fozil');
@@ -9,11 +9,72 @@ console.log(12345666799565656989687);
 console.log(12345666799565656989687n);
 
 var num = 100;
+var box = 35468461813876871687n
 num = 150;
+console.log(num,box);
+
+var num = 120;
+num = 150;
+var num = 200;
 console.log(num);
+
 
 let num = 100;
 num = 150;
 let num = 50;
 
+const num =150;
+num = 100;
+const num =120;
 console.log(num);
+
+console.log(1995);
+
+let obj = {
+    name: 'Fozil',
+    status: 'developer',
+    age: 28,
+}
+console.log(obj);
+
+function example() {
+    console.log('java');
+}
+example();
+
+const arr1 = [1, 2, 3, 5]
+
+console.log(arr1[3]);
+
+let nomalum = null;
+// let nomalum;
+console.log(nomalum);
+
+let name = 'fozil';
+let age = 28;
+let isAdult = true;
+let myNull = null;
+let myUndefined = undefined;
+let sym = Symbol('fozil');
+let big = BigInt(1995);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isAdult);
+console.log(typeof myNull);
+console.log(typeof myUndefined);
+console.log(typeof sym);
+console.log(typeof big);
+
+let arr = [1, 2,  3, 4, 5]
+console.log(typeof arr);
+
+hoisting
+console.log(str);
+var str = 'salom'
+
+console.log(str);
+let str = 'salom'
+
+console.log(str);
+let str = 'salom'
