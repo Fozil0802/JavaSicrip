7add59468abbcf804af42e2dd6e79a257c5f144b
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-11-07 23:57:33 +0900

    starting.JavaScrip

---
+++ js/lesson1/script.js
@@ -0,0 +1,19 @@
+// var hello = "salom, dunyolar";
+// console.log(hello);
+
+console.log(19950802);
+console.log('fozil');
+console.log(4>5);
+console.log(5<7);
+console.log(12345666799565656989687);
+console.log(12345666799565656989687n);
+
+var num = 100;
+num = 150;
+console.log(num);
+
+let num = 100;
+num = 150;
+let num = 50;
+
+console.log(num);
