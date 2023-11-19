cd56ccef5a395bc7e8d8e6288914da8dd90450ce
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-11-20 00:15:52 +0900

    math.first

---
+++ js/lesson5/index.js
@@ -0,0 +1,90 @@
+// begin1
+let a = 10;
+let P = 4 * a;
+console.log(P);
+
+// begin2
+
+let a1 = 10;
+// let s = `${a1 * a1}`;
+let s = a1 * a1;
+console.log(s);
+
+// begin3
+
+let a2 = 5;
+let b = 4;
+let S = a2 * b;
+let P1 = 2 *(a2 + b);
+console.log(P1);
+
+// begin4
+let d = 10;
+let L = 3.14 * d;
+console.log(L);
+
+// begin5
+
+let a3 = 5;
+let V = a3 * a3 * a3;
+let S1 = 6 * a3 * a3;
+console.log(V);
+console.log(S1);
+
+// begin6
+
+let a4 = 3;
+let b1 = 4;
+let c = 5;
+let V1 = a4 * b1 * c;
+let S2 = 2 * (a4 * b1 + b1 * c + a4 * c)
+ console.log(V1);
+ console.log(S2);
+
+// begin7
+
+let R = 10;
+let L1 = 2 * 3.14 * R;
+let S3 = 3.14 * R * R;
+console.log(L1);
+console.log(S3);
+
+// begin8
+
+let a5 = 5;
+let a6 = 6;
+let P2 = (a5 +a6)/2;
+console.log(P2);
+
+// begin9
+
+let a7 = 4;
+let b2 = 9;
+console.log((a7 * b2)**0.5);
+
+// begin10
+
+let a8 = 2;
+let b3 = 3;
+let Y = a8 + b3;
+let K = a8 * b3;
+console.log(Y **2);
+console.log(K **2);
+
+// begin11
+
+let a9 = 5;
+let b4 = 7;
+let Y1 = a9 + b4;
+let K1 = a9 * b4;
+console.log(Y1 % 2);
+console.log(K1 % 2);
+
+// begin12
+
+let a10 = 6;
+let b5 = 8;
+let c1 = (a10**2 + b5**2)**(1/2);
+let P3 = a10 + b5 + c1;
+console.log(c1);
+console.log(P3);
