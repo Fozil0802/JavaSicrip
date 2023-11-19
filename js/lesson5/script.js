a141340f5472d0eec3b10d05112ce0afa89c1a90
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-11-19 21:48:35 +0900

    repitition and number

---
+++ js/lesson5/script.js
@@ -0,0 +1,64 @@
+// while//dowhile
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i++
+// }
+
+// let i = 0;
+// while (i>1) {
+//     console.log(i);
+//     i--
+// }
+
+// let j = 0;
+// do{
+//     console.log(j);
+//     j++
+// }while(j>1)
+
+// break // continue
+
+// for(let i = 1; i<=5; i++){
+//     if(i == 3){
+//         break
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 1; i<=5; i++){
+//     if(i == 4){
+//         continue
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 1; i<=10; i++){
+//     if(i % 2 === 0){
+//         continue
+//     }
+//     console.log(i);
+// }
+
+// karakara
+
+// for(let i = 1; i<=10; i++){
+
+//     for(let j = 1; j <= 10; j++){
+//         console.log(`${i} x ${j} = ${i*j}`);
+//     }
+
+//     console.log('==================');
+// }
+
+// number data type
+
+// console.log(15+15);
+// console.log(15-15);
+// console.log(15/15);
+// console.log(15*15);
+// console.log(15%5);
+// console.log(15%4);
+// console.log(10**3);
+// console.log(9**(1/2));
