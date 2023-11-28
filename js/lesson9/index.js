4211ef3e36cc8bac216c139d9c22312f11a6d662
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-11-28 23:29:46 +0900

    Object.homework.next

--- js/lesson9/index.js
+++ js/lesson9/index.js
@@ -1,28 +1,47 @@
-// person = {
-//       id: 1,
-//       name: "Umar",
-//       surname: "Ismoilov",
-//       ielts: 5.5,
-//       student: true,
-//       parol: 101,
-//     };
-//     function Kirish(parol, name) {
-//         if(id === 1 && name ==="Umar"){
-//             console.log('Hush kelibsiz');
-//         }
-//         console.log('parol yoki');
-//     }
-//     console.log(Kirish);
 
 // 1-masala
 
-let obj = {
+let obj1 = {
   id: 1,
   name: "Webbrain",
 };
 
+for (let key in obj1) {
+  // console.log(key);
+}
 
 // 2-masala
+let obj2 = {
+  id: 1,
+  name: "Webbrain",
+};
+
+// console.log(obj2[key]);
+// console.log(key + ": " + obj2[key]);
+console.log("id: " + obj2.id);
+console.log("No3: " + obj2.No3);
+
+// 3-masala
+let obj = {
+  id: 1,
+  name: "WebBrain",
+  offline: true,
+  online: true,
+  individual: false
+};
+
+// Obyektning faqat boolean tipidagi maydonlarini ajratib olamiz
+let booleanValues = {};
+for (let key in obj) {
+  if (typeof obj[key] === "boolean") {
+    booleanValues[key] = obj[key];
+  }
+}
+
+// Natijani chiqaramiz
+console.log(booleanValues);
+
+// 4-masala
 
 let person = {
   id: 1,
@@ -50,3 +69,28 @@ while (true) {
   person = person.child;
 }
 console.log(sum);
+
+//5-masala
+
+let person1 = {
+  id: 1,
+  name: 'Umar',
+  surname: 'Ismoilov',
+  ielts: 5.5,
+  student: true,
+  parol: 101
+};
+
+function Kirish(parol, name) {
+  // Bazadagi ma'lumotlar
+  let bazadagiParol = person1.parol;
+  let bazadagiName = person1.name;
+
+  // Togri kirish tekshirish
+  if (parol === bazadagiParol && name === bazadagiName) {
+      console.log('Hush kelibsiz!');
+  } else {
+      console.log('Parol yoki ism xato');
+  }
+}
+Kirish();
