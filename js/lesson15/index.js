88e0dae64c416a199c85121923d5d7107a61c0c1
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-12-10 23:34:54 +0900

    prototype...

---
+++ js/lesson15/index.js
@@ -0,0 +1,6 @@
+(function () {
+    function example() {
+      console.log("hello Fozil");
+    }
+    example();
+  })();
--- js/lesson15/script.js
+++ js/lesson15/script.js
@@ -16,3 +16,43 @@
 //   console.log("hello Fozil");
 // })();
 
+// (function () {
+//   function example() {
+//     console.log("hello java");
+//   }
+//   example();
+// })();
+
+// prototype
+
+const user = {
+  firstname: "Fozil",
+  lastname: "Khudoyberdiev",
+  greeting() {
+    console.log("hello");
+  },
+};
+
+// const num = 10;
+const num = new Number(100);
+
+// const str = 'example';
+
+// const str = new String("example");
+// console.log(str);
+
+// const example = Object.create(user)
+
+// const user2 = {};
+
+// Object.prototype.getAge = function(){
+// console.log('i am 28 years old');
+
+// }
+// const user2 = [];
+
+// Array.prototype.example = function(){
+// console.log('i am 28 years old');
+// }
+
+// console.log(user2.example())
