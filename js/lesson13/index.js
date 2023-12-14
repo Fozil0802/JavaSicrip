fc13c7b10478c69cc57eed6d802ab6764dc428bd
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-12-14 23:47:01 +0900

    homework.min.max.delete...

--- js/lesson13/index.js
+++ js/lesson13/index.js
@@ -1,10 +1,33 @@
 // 1-masala
 
-const findsum = (n) => {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += i;
-  }
-  return sum;
+// const findsum = (n) => {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// };
+// console.log(findsum(10));
+
+// // 2-masala
+
+let arr1 = [4, 10, 88, 99, 100];
+
+let min = arr1.sort((a, b) => a - b)[0];
+let max = +arr1
+  .sort((a, b) => a - b)
+  .slice(-1)
+  .join();
+console.log(max);
+
+// 3-masala
+
+let arr = ["olma", "banan", "nok", "olma", "nok", "tarvuz"];
+
+const deleteMeva = (meva) => {
+arr = arr.filter( fruit => fruit !== meva );
 };
-console.log(findsum(10));
+deleteMeva("nok");
+console.log(arr);
+
+
