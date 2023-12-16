e37795d7d1ca4bd05aff08b4285842d43774d16a
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-12-16 22:09:06 +0900

    foor.loop

--- sammi.js/script.js
+++ sammi.js/script.js
@@ -1,55 +1,108 @@
 // let age = 28 // number
 // let myName = 'Fozil' // string
 // let bool = false// boolean
-let number;
-console.log(number);
+// let number;
+// console.log(number);
 
-// object
+// // object
 
-let person = {
+// let person = {
 
-    name: 'Fozil', // string
-    age: 28, // number
-    isMarried: false, //boleen
+//     name: 'Fozil', // string
+//     age: 28, // number
+//     isMarried: false, //boleen
 
-}
+// }
 
-console.log(person);
-
-
-//array
-
-let arr = ['mishka', 'tel'];
-arr[0]= 'decktop'
-console.log(arr);
+// console.log(person);
 
 
-let x = 12;
-let y = 6;
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x/y);
+// //array
 
-let z = 15;
-console.log(z%7);
+// let arr = ['mishka', 'tel'];
+// arr[0]= 'decktop'
+// console.log(arr);
 
-x=x+1
-x--
-x++
-console.log(x);
 
-let a = 10;
-let b = 10;
-console.log(a==b);
-console.log(10=='10')
+// let x = 12;
+// let y = 6;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
 
-console.log('5'+ 5);
+// let z = 15;
+// console.log(z%7);
+
+// x=x+1
+// x--
+// x++
+// console.log(x);
+
+// let a = 10;
+// let b = 10;
+// console.log(a==b);
+// console.log(10=='10')
+
+// console.log('5'+ 5);
 
 // alert('hello')
 
-let aneswers = confirm('fozil sizmi')
-console.log(aneswers);
+// let aneswers = confirm('fozil sizmi')
+// console.log(aneswers);
 
-let aneswer = prompt('siz kimisz')
-console.log(aneswer);
+// let aneswer = prompt('siz kimisz')
+// console.log(aneswer);
+
+//foor loop
+
+ //first
+//  for( let i =0; i < 10; i++){
+// console.log(i)
+//  }
+
+ //second
+
+// let i = 0;
+// do{
+// console.log(i);
+// }while(i < 10)
+
+
+// third
+
+// let i = 0;
+// while(i < 0){
+//     console.log(i);
+//     i++
+// }
+
+// let aneswes = [];
+// let questions = [
+//     'what is your name',
+//     'how old are you'
+// ]
+
+// for(let i = 0; i < questions.length; i++){
+//     aneswes[i] = prompt(questions[i])
+// }
+// console.log(aneswes);
+
+// let aneswes = [];
+// let questions = [
+//     'what is your name',
+//     'how old are you'
+// ]
+
+// let i = 0;
+// do{
+//     aneswes[i] = prompt(questions[i])
+// console.log(aneswes);
+// }while(i < questions.length)
+
+// let i = 0;
+// while(i < questions.length){
+//     aneswes[i] = prompt(questions[i])
+//     i++
+// }
+// console.log(aneswes);
