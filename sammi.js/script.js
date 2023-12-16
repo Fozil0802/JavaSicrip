4e5cce88ada2113536b69f50716ff45a56191b0f
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-12-16 21:07:22 +0900

    data type

---
+++ sammi.js/script.js
@@ -0,0 +1,55 @@
+// let age = 28 // number
+// let myName = 'Fozil' // string
+// let bool = false// boolean
+let number;
+console.log(number);
+
+// object
+
+let person = {
+
+    name: 'Fozil', // string
+    age: 28, // number
+    isMarried: false, //boleen
+
+}
+
+console.log(person);
+
+
+//array
+
+let arr = ['mishka', 'tel'];
+arr[0]= 'decktop'
+console.log(arr);
+
+
+let x = 12;
+let y = 6;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+let z = 15;
+console.log(z%7);
+
+x=x+1
+x--
+x++
+console.log(x);
+
+let a = 10;
+let b = 10;
+console.log(a==b);
+console.log(10=='10')
+
+console.log('5'+ 5);
+
+// alert('hello')
+
+let aneswers = confirm('fozil sizmi')
+console.log(aneswers);
+
+let aneswer = prompt('siz kimisz')
+console.log(aneswer);
