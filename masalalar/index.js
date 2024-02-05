e05d442225215a7101b12b601124afeb7a89230b
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2024-02-05 21:55:07 +0900

    practice

--- masalalar/index.js
+++ masalalar/index.js
@@ -1,41 +1,73 @@
-//  KARAKARA
-for (let i = 1; i <= 10; i++) {
-  for (let j = 1; j <= 10; j++) {
-    console.log(`${i} x ${j} = ${i * j}`);
-  }
+// //  KARAKARA
+// for (let i = 1; i <= 10; i++) {
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(`${i} x ${j} = ${i * j}`);
+//   }
 
-  console.log("==================");
-}
+//   console.log("==================");
+// }
 
-// juft va toq son
- let num= 15; 
-function evenOrOdd(number) {
-  return number % 2 === 0 ? "Even" : "Odd";
-}
-console.log(evenOrOdd(num));
-//  3-masala
-let arr = [1, -4, 7, 12];
-function positiveSum(arr) {
-  let newsum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      newsum += arr[i];
-    }
-  }
-return newsum
-}
+// // juft va toq son
+//  let num= 15; 
+// function evenOrOdd(number) {
+//   return number % 2 === 0 ? "Even" : "Odd";
+// }
+// console.log(evenOrOdd(num));
+// //  3-masala
+// let arr = [1, -4, 7, 12];
+// function positiveSum(arr) {
+//   let newsum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       newsum += arr[i];
+//     }
+//   }
+// return newsum
+// }
 
-console.log(positiveSum(arr));
+// console.log(positiveSum(arr));
 
-// 4-masala
+// // 4-masala
 
-let str = "world";
+// let str = "world";
 
-function solution(str) {
-  return str.split("").reverse().join("");
-}
+// function solution(str) {
+//   return str.split("").reverse().join("");
+// }
 
-solution(str); 
+// solution(str); 
 
+// // 5-masala
+
+// let number = 12;
+// function opposite(number) {
+//   return number * -1;
+// }
+
+// console.log(opposite(number));
+
+// // 6-masala
+// let num = 123;
+// function numb(num) {
+//   return num.toString();
+// }
+// console.log(numb(typeof "num"));
+
+// // 7-masala
+
+// function boolToWord(bool) {
+//   return bool ? "Yes" : "No";
+// }
+
+// console.log(boolToWord());
+
+// // 8-masala
+
+// let str = "world";
+
+// function solution(str) {
+//   return str.split("").reverse().join("");
+// }
+// console.log(solution(str));
 
         
